5981c61b11ee2b05828cab1072e6d62459943139
jademeyer <jadekmeyer@gmail.com>
2018-05-11 09:57:31 -0400

    Update PropertySpace.java

--- PropertySpace.java
+++ PropertySpace.java
@@ -1,11 +1,13 @@
 public class PropertySpace extends Space {
   
+  private int position;
   private int price;
   private int rent;
   private int mortgage;
   
-  public Property(int p, int r, int m) {
-    price = p;
+  public Property(int pos, int pr, int r, int m) {
+    position = pos;
+    price = pr;
     rent = r;
     mortgage = m;
   }
