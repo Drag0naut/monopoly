442f60a19b4341e53755f55a0430d5d0d93821d2
jademeyer <jadekmeyer@gmail.com>
2018-05-11 08:48:06 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,25 @@
 
 public class Player {
-
-}
+  
+  private String name;
+  private int money;
+  private int properties;
+  private int position;
+  
+  public Player(String n) {
+    name = n;
+    money = 1500;
+    properties = 0;
+    position = 0;
+  }
+  
+  public void move() {
+    int r1 = (int) (Math.random() * 6) + 1;
+		int r2 = (int) (Math.random() * 6) + 1;
+    int newPosition = position + r1 + r2;
+		if (newPosition <= 40) {
+      position = newPosition;
+    } else {
+      position = 40 - newPosition;
+    }   
+  }
