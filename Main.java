7a3ab110b93f53ed001000b404e1dc36abd9ddce
jademeyer <jadekmeyer@gmail.com>
2018-05-11 10:00:16 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+
+  Board board;
+  Space[] spaces = new Space[40];
+  PropertySpace ... //initializes all of them
+  spaces = {}
+  board = new Board(spaces);
+
+}
