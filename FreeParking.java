615e2ca82731076d2d61b0cf6359a03eabbcaea9
jademeyer <jadekmeyer@gmail.com>
2018-05-16 09:55:09 -0400

    Update FreeParking.java

--- FreeParking.java
+++ FreeParking.java
@@ -1,4 +1,19 @@
 
-public class FreeParking {
+public class FreeParking implements Space {
 
+  private static int money;
+  
+  public FreeParking() {
+    money = 100;
+  }
+  
+  public static void act(Player p) {
+    p.changeMoney(money);
+    money = 100;
+  }
+  
+  public static void changeMoney(int tax) {
+    money += tax;
+  }
+  
 }
