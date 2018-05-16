390408c2eff7d2b3e85ac6119aa55fb1f41ea124
jademeyer <jadekmeyer@gmail.com>
2018-05-16 10:14:34 -0400

    Update Property.java

--- Property.java
+++ Property.java
@@ -2,8 +2,8 @@
 public class Property implements Square{
 	private String name;
 	private String owner;
-	private boolean mortgageState;
-	private int numberofHouses;
+	private boolean isMortgaged;
+	private int numHouses;
 	private String color;
 	private static final int RENT;
 	private static final int RENT_MONOPOLY;
@@ -12,13 +12,18 @@ public class Property implements Square{
 	private static final int RENT_THREEHOUSE;
 	private static final int RENT_FOURHOUSE;
 	private static final int RENT_HOTEL;
-	public Property()
+	public Property(String n, String c, ...//rents)
 	{
-		//initialize variables
+		name = n;
+		owner = "";
+		isMortgaged = false;
+		numHouses = 0;
+		color = c;
+		...//rents
 	}
 	public void setOwner()
 	{
-		
+
 	}
 	public void changeMortgageState()
 	{
@@ -26,14 +31,24 @@ public class Property implements Square{
 	}
 	public void addHouses()
 	{
-		
+		numHouses++;
 	}
 	public void removeHouses()
 	{
-		
+		numHouses--;
 	}
-	public void act()
+	public void act(Player p)
 	{
-		
+		if (owner.equals("")) {
+			Scanner in = new Scanner(System.in);
+			System.out.println("Purchase property? y/n");
+			String ans = in.next();
+			if (ans.equals("y")) {
+			owner = p.getName();
+			}
+		} else if (!p.getName().equals(owner)) {
+			... //add cases that determine rent amt
+			p.changeMoney(0 - RENT);
+		}
 	}
 }
