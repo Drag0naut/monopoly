462d4bb71d2b9f563308e5eb65bb3bc33ad4a683
chucknorris2090 <39107812+chucknorris2090@users.noreply.github.com>
2018-05-17 08:42:23 -0400

    Update Utilities.java

--- Utilities.java
+++ Utilities.java
@@ -1,4 +1,43 @@
 
-public class Utilities {
-
+public class Utilities implements Space {
+	private String name;
+	private Player owner;
+	private boolean mortgageState;
+	private static final int COST;
+	private static final int MORTGAGE;
+	private static final int UNMORTGAGE;
+	private static final int RENT_MULTIPLIER;
+	
+	public Utilities(String namein, int cost, int mortgage, int unmortgage, int multiplier)
+	{
+		name = namein;
+		COST = cost;
+		MORTGAGE = mortgage;
+		UNMORTGAGE = unmortgage;
+		RENT_MULTIPLIER = multiplier;
+		mortgageState = true;
+	}
+	public void setOwner(String own)
+	{
+		owner = own;
+	}
+	public void changeMortgageState()
+	{
+		mortgageState  = !mortgageState;
+		// how to give back or take money tho?	
+	}
+	public void act(Player p)
+	{
+		if (owner == null)
+		{
+			p.askBuy(); //ask the user if they want to buy the property
+		}
+		else
+		{
+			int transfer = (p.roll1() + p.roll2()) * RENT_MULTIPLIER;
+			p.getMoney(transfer); // removes money from player
+			owner.addMoney(transfer);
+		}
+	}
+	
 }
