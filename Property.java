d45635e206dd914e1d4f1f02968e6aaf6eb0766d
ch-alina <chen.alina73@gmail.com>
2018-05-18 08:19:11 -0400

    Update Property.java

--- Property.java
+++ Property.java
@@ -5,21 +5,29 @@ public class Property implements Square{
 	private boolean isMortgaged;
 	private int numHouses;
 	private String color;
-	private static final int RENT;
-	private static final int RENT_MONOPOLY;
-	private static final int RENT_ONEHOUSE;
-	private static final int RENT_TWOHOUSE;
-	private static final int RENT_THREEHOUSE;
-	private static final int RENT_FOURHOUSE;
-	private static final int RENT_HOTEL;
-	public Property(String n, String c, ...//rents)
+	private int rent;
+	private final int RENT_ORIGINAL;
+	private final int RENT_MONOPOLY;
+	private final int RENT_ONEHOUSE;
+	private final int RENT_TWOHOUSE;
+	private final int RENT_THREEHOUSE;
+	private final int RENT_FOURHOUSE;
+	private final int RENT_HOTEL;
+	public Property(String n, String col, int rOrig, int rMono, int rOne, int rTwo, int rThree, int rFour, int rHotel)
 	{
 		name = n;
 		owner = "";
 		isMortgaged = false;
 		numHouses = 0;
 		color = c;
-		...//rents
+		rent = rOrig;
+		RENT_ORIGINAL = rOrig;
+		RENT_MONOPOLY = rMono;
+		RENT_ONEHOUSE = rOne;
+		RENT_TWOHOUSE = rTwo;
+		RENT_THREEHOUSE = rThree;
+		RENT_FOURHOUSE = rFour;
+		RENT_HOTEL = rHotel;	
 	}
 	public void setOwner()
 	{
@@ -27,7 +35,12 @@ public class Property implements Square{
 	}
 	public void changeMortgageState()
 	{
-		
+		if (isMortgaged) {
+			isMortgaged = false;
+		}
+		else {
+			isMortgaged = true;
+		}
 	}
 	public void addHouses()
 	{
@@ -49,6 +62,7 @@ public class Property implements Square{
 		} else if (!p.getName().equals(owner)) {
 			... //add cases that determine rent amt
 			p.changeMoney(0 - RENT);
-		}
+			
+			//need else if mortgaged 
 	}
 }
