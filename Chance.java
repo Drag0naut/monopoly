a0aed155e97336ad4d62b05f917fd22a7495c841
chucknorris2090 <39107812+chucknorris2090@users.noreply.github.com>
2018-05-21 08:45:36 -0400

    Update Chance.java

--- Chance.java
+++ Chance.java
@@ -7,15 +7,25 @@ public class Chance implements Space {
         }
         if (ran == 1)
         {
-            p.goTo(IllinoisAvenue); //go to  Illinois Ave.
+            p.goTo(x); //x = illinoisave, go to  Illinois Ave.
         }
         if (ran == 2)
         {
-            p.goTo(StCharlesPlace); // go to st charles place
+            p.goTo(x); // x = stcharlesplace, go to st charles place
         }
         if (ran == 3)
         {
-            // go to  closest utility,  if owned, pay ten  times dice roll
+        	//implement the roll
+            if (p.getPos() < x || p.getPos() > y) // x = pos of first util, y = pos of last util
+            {
+            	p.move(x);
+            }
+            else
+            {
+            	p.move(y);
+            }
+            	
+            	// go to  closest utility,  if owned, pay ten  times dice roll
         }
         if (ran == 4)
         {
@@ -23,49 +33,59 @@ public class Chance implements Space {
         }
         if (ran == 5)
         {
-            //give 50 bucks to  player
+            p.changeMoney(50);//give 50 bucks to  player
         }
         if (ran == 6)
         {
-            //get out of jail  free card
+            p.addGetOutOfJailFreeCard();//get out of jail  free card
         }
         if (ran == 7)
         {
-            // go back  3 spaces
+            p.move(p.getPos() - 3);// go back  3 spaces
         }
         if (ran == 8)
         {
-            // go  to jail
+            p.move();// go  to jail
         }
         if (ran == 9)
         {
-            //25 bucks foreach house, 100 for each hotel owned
+        	int i = 0;
+        	i += p.getHouses() * 25;
+        	i += p.getHotels() * 100;
+            p.changeMoney(0-i); //25 bucks foreach house, 100 for each hotel owned
         }
         if (ran == 10)
         {
-            // 15 $ tax
+            p.changeMoney(0-15);// 15 $ tax
         }
         if (ran == 11)
         {
-            //go to reading railroad, if pass go,  collect 200
+        	int i = ; // whatever reading railroad is in the array
+        	if (p.getPos()) > i) {p.changeMoney(200);}
+            p.move(i); //go to reading railroad, if pass go,  collect 200
         }
         
         if (ran == 12)
         {
-            // go  to  boardwalk
+            p.move(); // go  to  boardwalk
         }
         if (ran == 13)
         {
+            p.changeMoney(0-50);
+            ArrayList<Player> temp = getPlayerList();
+            for (int i = 0; i < temp.size(); i++)
+            {
+            	if (!(temp.get(i).equals(p))) {temp.get(i).changeMoney(50);}
+            }
             //pay each player $50
         }
         if (ran == 14)
         {
-            p.addMoney(150); //collect $150
+            p.changeMoney(150); //collect $150
         }
         if (ran == 15)
         {
-            //collect $100
+            p.changeMoney(100);//collect $100
         }
         
     }
-}
