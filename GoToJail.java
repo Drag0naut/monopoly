8e2cf07dbdd95c8ce5f531dc2011d5d6f7bbb651
chucknorris2090 <39107812+chucknorris2090@users.noreply.github.com>
2018-05-22 18:30:39 -0400

    Update GoToJail.java

--- GoToJail.java
+++ GoToJail.java
@@ -1,6 +1,11 @@
 
 public class GoToJail implements Space {
-
+  private int position;
+  public GoToJail(pos)
+  {
+    position = pos;
+  }
+  
   public void act(Player p) {
     p.goToJail();
   }
