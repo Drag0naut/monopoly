4b1c5cb99230db8882be2f547a7525ec9e5331ce
chucknorris2090 <39107812+chucknorris2090@users.noreply.github.com>
2018-05-22 19:36:14 -0400

    Update Railroad.java

--- Railroad.java
+++ Railroad.java
@@ -1,9 +1,9 @@
-
+import java.util.Scanner;
 public class Railroad implements Space {
   
   private int position;
   private String name;
-  private String owner;
+  private Player owner;
   private boolean isMortgaged;
   private static int numOwned;
   private int price;
@@ -13,7 +13,7 @@ public class Railroad implements Space {
   public Railroad(int p, String n) {
     position = p;
     name = n;
-    owner = "";
+    owner = null;
     isMortgaged = false;
     numOwned = 0;
     price = 200;
@@ -22,14 +22,47 @@ public class Railroad implements Space {
   }
   
   public void act(Player p) {
-    if (owner.equals("")) {
-      if (p.askToBuy(getName())) {
-        numOwned++;
-        rent *= numOwned;
-      }
-    } else if (!owner.equals(p.getName())) {
-      p.changeMoney(0 - rent);
-    }
+	  if (owner == null)
+		{
+			Scanner in = new Scanner(System.in);
+			System.out.println("Purchase property? y/n");
+			String ans = in.next();
+			boolean found = false;
+			while (!found)
+			{
+				if (ans.equals("y"))
+				{
+					owner = p;
+					found = true;
+					p.addRailroad();
+				}
+				// case for n
+			}
+			in.close();
+		}
+	  else if (!(p == owner) && !isMortgaged)
+	  {
+		  if (owner.getRailroad() == 1)
+		  {
+			  p.changeMoney(0-25);
+			  owner.changeMoney(25);
+		  }
+		  else if (owner.getRailroad() == 2)
+		  {
+			  p.changeMoney(0-50);
+			  owner.changeMoney(50);
+		  }
+		  else if (owner.getRailroad() == 3)
+		  {
+			  p.changeMoney(0-100);
+			  owner.changeMoney(100);
+		  }
+		  else 
+		  {
+			  p.changeMoney(0-200);
+			  owner.changeMoney(200);
+		  }
+	  }
   }
   
   public String getName() {
