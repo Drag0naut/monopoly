6c2222db7cfe534637a740a59e032502a8f3aa87
chucknorris2090 <39107812+chucknorris2090@users.noreply.github.com>
2018-05-22 19:09:34 -0400

    Update Property.java

--- Property.java
+++ Property.java
@@ -1,12 +1,12 @@
-public class Property implements Square{
+import java.util.Scanner;
+public class Property implements Space{
 	private int position;
 	private String name;
-	private String owner;
+	private Player owner;
 	private boolean isMortgaged;
 	private int numHouses;
 	private String color;
 	private int price;
-	private int rent;
 	private final int RENT_ORIGINAL;
 	private final int RENT_MONOPOLY;
 	private final int RENT_ONEHOUSE;
@@ -15,18 +15,18 @@ public class Property implements Square{
 	private final int RENT_FOURHOUSE;
 	private final int RENT_HOTEL;
 	private int houseCost;
+	private int houseCount;
 	private int mortgageAmt;
 	
 	public Property(int p, String n, String col, int pr, int rOrig, int rMono, int rOne, int rTwo, int rThree, int rFour, int rHotel, int hC, int mA)
 	{
 		position = p;
 		name = n;
-		owner = "";
+		owner = null;
 		isMortgaged = false;
 		numHouses = 0;
-		color = c;
+		color = col;
 		price = pr;
-		rent = rOrig;
 		RENT_ORIGINAL = rOrig;
 		RENT_MONOPOLY = rMono;
 		RENT_ONEHOUSE = rOne;
@@ -60,17 +60,55 @@ public class Property implements Square{
 	}
 	public void act(Player p)
 	{
-		if (owner.equals("")) {
+		if (owner == null)
+		{
 			Scanner in = new Scanner(System.in);
 			System.out.println("Purchase property? y/n");
 			String ans = in.next();
-			if (ans.equals("y")) {
-			owner = p.getName();
+			boolean found = false;
+			while (!found)
+			{
+				if (ans.equals("y"))
+				{
+					owner = p;
+					found = true;
+				}
+				// case for n
 			}
-		} else if (!p.getName().equals(owner)) {
-			... //add cases that determine rent amt
-			p.changeMoney(0 - RENT);
-			
-			//need else if mortgaged 
+		}
+		else if (!(p == owner) && !isMortgaged)
+		{
+			if (houseCount == 0) 
+			{
+				p.changeMoney(0 - RENT_ORIGINAL);
+				owner.changeMoney(RENT_ORIGINAL);
+			}
+			//need case for monopoly, but no houses
+			if (houseCount == 1) 
+			{
+				p.changeMoney(0 - RENT_ONEHOUSE);
+				owner.changeMoney(RENT_ONEHOUSE);
+			}
+			if (houseCount == 2) 
+			{
+				p.changeMoney(0 - RENT_TWOHOUSE);
+				owner.changeMoney(RENT_TWOHOUSE);
+			}
+			if (houseCount == 3) 
+			{
+				p.changeMoney(0 - RENT_THREEHOUSE);
+				owner.changeMoney(RENT_THREEHOUSE);
+			}
+			if (houseCount == 4) 
+			{
+				p.changeMoney(0 - RENT_FOURHOUSE);
+				owner.changeMoney(RENT_FOURHOUSE);
+			}
+			if (houseCount == 5) 
+			{
+				p.changeMoney(0 - RENT_HOTEL);
+				owner.changeMoney(RENT_HOTEL);
+			}
+		}
 	}
 }
