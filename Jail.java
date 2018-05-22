e697058b769134ba8f26b1e2130bb9f76a290be6
chucknorris2090 <39107812+chucknorris2090@users.noreply.github.com>
2018-05-21 23:00:46 -0400

    Update Jail.java

--- Jail.java
+++ Jail.java
@@ -1,4 +1,10 @@
 
-public class Jail {
-
+public class Jail implements Space {
+	public void act(Player p)
+	{
+		if (p.isInJail()) 
+		{
+			//give option to roll doubles or pay, up jail turn if staying
+		}
+	}
 }
