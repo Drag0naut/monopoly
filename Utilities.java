bd1bc62e724a99fb00ff9b935870c690a268c5ed
chucknorris2090 <39107812+chucknorris2090@users.noreply.github.com>
2018-05-21 21:54:08 -0400

    Update Utilities.java

--- Utilities.java
+++ Utilities.java
@@ -1,4 +1,16 @@
 
 public class Utilities {
-
+	public void act(Player p)
+	{
+		if (this.getOwner().getUtilities() == 2)
+		{
+			p.changeMoney(0 - p.getRoll() * 10);
+			this.getOwner().changeMoney(p.getRoll() * 10);
+		}
+		else
+		{
+			p.changeMoney(0 - p.getRoll() * 4);
+			this.getOwner().changeMoney(p.getRoll() * 4);
+		}
+	}
 }
